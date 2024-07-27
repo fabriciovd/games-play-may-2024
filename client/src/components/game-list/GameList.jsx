cd0e1779a6f6fced3aabf2f1be9a255659cd2fa8
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-07-27 14:32:57 +0300

    Added no list items

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -12,7 +12,11 @@ export default function GameList(props) {
     <>
       <section id="catalog-page">
         <h1>All Games</h1>
-        {games.length > 0 ? games.map((game) => (<GameListItem key={game._id} {...game} />)) : <h3 className="no-articles">No articles yet</h3>}
+        {games.length > 0 ? (
+          games.map((game) => <GameListItem key={game._id} {...game} />)
+        ) : (
+          <h3 className="no-articles">No articles yet</h3>
+        )}
       </section>
     </>
   );
