2ba57293fc9babdb547e11c35bd49c6b516838fd
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-07-27 14:32:24 +0300

    Add no games message

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -12,11 +12,7 @@ export default function GameList(props) {
     <>
       <section id="catalog-page">
         <h1>All Games</h1>
-       {games.map((game) => (
-          <GameListItem key={game._id} {...game} />
-        ))}
-
-        <h3 className="no-articles">No articles yet</h3>
+        {games.length > 0 ? games.map((game) => (<GameListItem key={game._id} {...game} />)) : <h3 className="no-articles">No articles yet</h3>}
       </section>
     </>
   );
