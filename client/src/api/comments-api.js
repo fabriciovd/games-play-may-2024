ee1f1e941331dfdb067967da6cf3e1fb51925fff
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-07-28 10:01:25 +0300

    Create useForm custom hook

--- client/src/api/comments-api.js
+++ client/src/api/comments-api.js
@@ -1,18 +1,18 @@
-import requester from "./requester"
+import requester from "./requester";
 
 const BASE_URL = "http://localhost:3030/jsonstore/games";
 
-const buldUrl = (gameId)=>`${BASE_URL}/${gameId}/comments`
+const buldUrl = (gameId) => `${BASE_URL}/${gameId}/comments`;
 
-const create = (gameId, username, text)=>requester.post(buldUrl(gameId), {username, text});
+const create = (gameId, username, text) => requester.post(buldUrl(gameId), { username, text });
 
-const getAll = async(gameId)=>{
-    const result = requester.get(buldUrl(gameId));
-    const comments = Object.values(result);
-    return comments;
-}
+const getAll = async (gameId) => {
+  const result = await requester.get(buldUrl(gameId));
+  const comments = Object.values(result);
+  return comments;
+};
 
 export default {
-    create,
-    getAll,
-}
+  create,
+  getAll,
+};
