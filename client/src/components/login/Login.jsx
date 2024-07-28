1dce821e3df618e0a164ab59e8e8423a8b8e719f
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-07-28 12:01:50 +0300

    Add global authState

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,55 +1,60 @@
 import { useNavigate } from "react-router-dom";
 import { useLogin } from "../../hooks/useAuth";
-import { useForm } from "../../hooks/useFrom"
+import { useForm } from "../../hooks/useFrom";
+
+const initialValues = { email: "", password: "" };
 
 export default function Login(props) {
-    const login = useLogin();
-    const navigate = useNavigate();
-    const {values, changeHandler, submitHandler} = useForm(
-        {email: "", password: ""}, 
-        ({email, password})=>{
-           
-            login(email,password).then((res)=>{
+  const login = useLogin();
+  const navigate = useNavigate();
+  const loginHandler = async ({ email, password }) => {
+    login(email, password)
+      .then((res) => {
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  const { values, changeHandler, submitHandler } = useForm(
+    initialValues,
+    loginHandler
+  );
 
-                navigate("/")
-            }).catch((err)=>{
-                console.log(err.message);
-            })
-        }
-    );
-    return(
-        <>
-            <section id="login-page" className="auth">
-            <form id="login" onSubmit={submitHandler}>
+  return (
+    <>
+      <section id="login-page" className="auth">
+        <form id="login" onSubmit={submitHandler}>
+          <div className="container">
+            <div className="brand-logo"></div>
+            <h1>Login</h1>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={changeHandler}
+              value={values.email}
+              placeholder="Sokka@gmail.com"
+            />
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input 
-                    type="email" 
-                    id="email" 
-                    name="email" 
-                    onChange={changeHandler}
-                    value={values.email}
-                    placeholder="Sokka@gmail.com"
-                    />
-
-                    <label htmlFor="login-pass">Password:</label>
-                    <input 
-                    type="password" 
-                    id="login-password" 
-                    name="password"
-                    value={values.password}
-                    onChange={changeHandler}
-                    />
-                    <input type="submit" className="btn submit" value="Login"/>
-                    <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-        </>
-    )
+            <label htmlFor="login-pass">Password:</label>
+            <input
+              type="password"
+              id="login-password"
+              name="password"
+              value={values.password}
+              onChange={changeHandler}
+            />
+            <input type="submit" className="btn submit" value="Login" />
+            <p className="field">
+              <span>
+                If you don't have profile click <a href="#">here</a>
+              </span>
+            </p>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,13 +1,14 @@
+import { useContext } from "react";
+
 import { login } from "../api/auth-api";
+import { AuthContext } from "../contexts/authContext";
 
 export const useLogin = (email, password) => {
+  const { changeAuthState } = useContext(AuthContext);
+
   const loginHandler = async (email, password) => {
-    try {
-      const result = await login(email, password);
-      console.log(result);
-    } catch (err) {
-      console.log(err.message);
-    }
+    const result = await login(email, password);
+    changeAuthState(result);
   };
 
   return loginHandler;
